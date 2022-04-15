1ea12d324cee88ef2d5b55875864ac3f9f110c51
Jean-Christophe Sicotte-Brisson <jeanchristophesicottebrisson@gmail.com>
2022-04-15 12:37:36 -0400

    Firmware complete

--- ElectroDice_Firmware/src/main.cpp
+++ ElectroDice_Firmware/src/main.cpp
@@ -1,9 +1,147 @@
-#include <Arduino.h>
+//***********************************************************************************
+// Copyright 2022 JCSB1994
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-void setup() {
-  // put your setup code here, to run once:
+//***********************************************************************************
+//
+// Description:
+//    This project operates an electronic dice that produces random numbers when shaken.
+//    LEDs light up to show the dice roll.
+//
+//    This Project demonstrates the use of C language synthax and direct register manipulation
+//    on the ATtiny13A microcontroller.
+//
+//***********************************************************************************
+
+#include <Arduino.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
+
+/**************************************************************************************************************************************************************/
+////CONSTANTS & VARIABLES
+/**************************************************************************************************************************************************************/
+
+#define SHIFT_REG_DATA_PIN  (3)
+#define SHIFT_REG_LATCH_PIN (0)
+#define SHIFT_REG_CLOCK_PIN (1)
+
+#define VIBRATION_SWITCH_PIN (2)
+#define RANDOM_SEED_ANALOG_INPUT (A1)
+
+#define DICE_MAX_SIDE_NB    (6)
+
+uint8_t diceSides[DICE_MAX_SIDE_NB] = {
+  0B1,
+  0B11000,
+  0B11001,
+  0B1011010,
+  0B1011011,
+  0B1111110
+};
+
+ISR(PCINT0_vect)
+{
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void updateShiftRegister(uint8_t regValue)
+{
+  PORTB &= ~(1 << SHIFT_REG_LATCH_PIN);
+
+  // Shift out: bit bang the register value from data pin with a clock cycle on each bit
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    digitalWrite(SHIFT_REG_DATA_PIN, !!(regValue & (1 << i)));
+    // Bit bang a clock cycle
+    PORTB |= (1 << SHIFT_REG_CLOCK_PIN);
+    PORTB &= ~(1 << SHIFT_REG_CLOCK_PIN);
+  }
+  PORTB |= (1 << SHIFT_REG_LATCH_PIN);
+}
+
+
+void diceRoll()
+{
+  uint8_t registerValue = 0;
+  uint8_t i = 0;
+  
+  for (; i <= DICE_MAX_SIDE_NB; i++)
+  {
+    registerValue = 1 << i;
+    updateShiftRegister(registerValue);
+    delay(8);
+  }
+
+  for (; i != 0; i--)
+  {
+    registerValue = 1 << i;
+    updateShiftRegister(registerValue);
+    delay(10);
+  }
+
+  int rollValue = analogRead(RANDOM_SEED_ANALOG_INPUT);
+
+  rollValue %= DICE_MAX_SIDE_NB;
+
+  registerValue = diceSides[rollValue];
+
+  updateShiftRegister(registerValue);
+  delay(200);
+}
+
+/**************************************************************************************************************************************************************/
+////INITIALIZATION
+/**************************************************************************************************************************************************************/
+
+
+void setup()
+{
+  // Set Shift register GPIOs as outputs
+  DDRB |= 1 << SHIFT_REG_CLOCK_PIN;
+  DDRB |= 1 << SHIFT_REG_LATCH_PIN;
+  DDRB |= 1 << SHIFT_REG_DATA_PIN;
+
+  // Set vibration switch GPIO as input with pullup enabled
+  DDRB &= ~(1 << VIBRATION_SWITCH_PIN);
+  PORTB |= (1 << VIBRATION_SWITCH_PIN);
+
+  // Enable interrupts, enable input state change interrupt on GPIO 2
+  GIMSK |= (1 << PCIE);
+  PCMSK |= _BV(PCINT2);
+  sei();
+
+  // sleep setup
+  sleep_enable();
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+}
+
+/**************************************************************************************************************************************************************/
+////LOOP
+/**************************************************************************************************************************************************************/
+
+void loop()
+{
+  // Set outputs to low
+  PORTB &= ~(1 << SHIFT_REG_CLOCK_PIN);
+  PORTB &= ~(1 << SHIFT_REG_LATCH_PIN);
+  PORTB &= ~(1 << SHIFT_REG_DATA_PIN);
+
+  ADCSRA &= ~_BV(ADEN); // Turn ADC off, saves ~230uA
+  sleep_cpu();
+  ADCSRA |= _BV(ADEN); // Turn ADC back on
+
+
+  diceRoll();
+
+  updateShiftRegister(0);
+
+  sleep_cpu();
+
 }
